f531a3c8e3f99817fe5826726773f8fb91ca2364
Duksoon <sergioromerogus7@gmail.com>
2024-09-16 06:17:54 -0500

    Finalizado V1.

--- Tears Of Misery sql.sql
+++ Tears Of Misery sql.sql
@@ -1,6 +1,7 @@
 create database tearsOfMisery;
 use tearsOfMisery;
 
+
 create table roles (
 idRol int primary key auto_increment NOT NULL unique,
 nombreRol varchar (20) NOT NULL unique 
@@ -27,41 +28,33 @@ create table producto (
 idProducto int primary key auto_increment NOT NULL unique,
 nombre varchar (50) NOT NULL,
 descripcion varchar (50) NOT NULL,
-talla ENUM('S','M','L','XL') ,
 precio float NOT NULL,
-stock int NOT NULL, 
 imagen varchar (255) NOT NULL,
 idCat int NOT NULL,
 foreign key (idCat) references categoria (idCat)
 );
 
-create table pedido (
-idPedido int primary key auto_increment NOT NULL unique,
-fechaPedido date NOT NULL,
-totalPedido float NOT NULL,
-estado varchar (40) NOT NULL,
-idUsuario int NOT NULL, 
-foreign key (idUsuario) references usuario (idUsuario)
-);
-
-CREATE TABLE pedido_producto (
-    idPedidoProducto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    idPedido INT NOT NULL,
+CREATE TABLE producto_tallas (
+    idTalla INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
     idProducto INT NOT NULL,
-    cantidad INT NOT NULL,
-    FOREIGN KEY (idPedido) REFERENCES pedido(idPedido),
-    FOREIGN KEY (idProducto) REFERENCES producto(idProducto)
+    talla ENUM('S', 'M', 'L', 'XL') NOT NULL,
+    stock INT NOT NULL,
+    FOREIGN KEY (idProducto) REFERENCES producto(idProducto) ON DELETE CASCADE
 );
 
 
-create table detallePedido (
-idDetalle int primary key auto_increment NOT NULL unique,
-cantidad int NOT NULL,
-precio float NOT NULL,
-idProducto int NOT NULL,
-foreign key (idProducto) references producto (idProducto),
-idPedido int NOT NULL,
-foreign key (idPedido) references pedido (idPedido)
+CREATE TABLE historial_compras (
+    idCompra INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+    idUsuario INT NOT NULL,
+    idProducto INT,
+    nombreProducto VARCHAR(50) NOT NULL,
+    imagenProducto VARCHAR(255) NOT NULL,
+    fechaCompra DATE NOT NULL,
+    cantidad INT NOT NULL,
+    total FLOAT NOT NULL,
+    estado ENUM('Pendiente', 'Enviado', 'Cancelado') DEFAULT 'Pendiente',
+    FOREIGN KEY (idUsuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (idProducto) REFERENCES producto(idProducto) ON DELETE SET NULL
 );
 
 insert into roles (nombreRol) values ('Administrador');
@@ -73,8 +66,12 @@ insert into categoria (nombre, descripcion) values ('Camisas', 'Negra con rayas'
 insert into categoria (nombre, descripcion) values ('Discos', 'Disco pionero');
 insert into categoria (nombre, descripcion) values ('Sacos', 'Sin capota');
 
-select * from usuario;
-select * from categoria;
-select * from pedido;	
-select * from pedido_producto;
-select * from producto;
+SELECT * FROM usuario;
+SELECT * FROM categoria;
+SELECT * FROM producto;
+SELECT * FROM historial_compras;
+SELECT * FROM producto_tallas;
+
+
+
+
