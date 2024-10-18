60cf6f84a092b1812790deec7a758d485aae551b
Solaya777 <solaya88@soy.sena.edu.co>
2024-10-18 11:32:35 -0500

    Arreglo de validaciones y función de correo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importaciones
 import MySQLdb
 from flask import Flask, abort, render_template, flash, jsonify, request, send_from_directory,session,redirect, url_for
+from flask_mail import Mail, Message
 from mvc.model.db_connection import create_connection, close_connection
 from mvc.controller.producto_controller import producto_controller
 from mvc.controller.usuarios_controller import usuarios_controller, verificar_usuario
@@ -29,8 +30,15 @@ app.register_blueprint(logout_controller)
 app.register_blueprint(pedidos_controller)
 app.register_blueprint(compras_controller)
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com' 
+app.config['MAIL_PORT'] = 587  
+app.config['MAIL_USE_TLS'] = True  
+app.config['MAIL_USE_SSL'] = False 
+app.config['MAIL_USERNAME'] = 'tearsofmisery.13@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'oaif rsnb gchj mkxh'  
+app.config['MAIL_DEFAULT_SENDER'] = ('TearsOfMisery', 'tearsofmisery.13@gmail.com')
 
-
+mail = Mail(app)
 
 
 
@@ -491,6 +499,7 @@ def actualizar_stock():
 
 
 
+
     
 
 if __name__ == '__main__':
@@ -499,3 +508,4 @@ if __name__ == '__main__':
         app.run(debug=True)
     except Exception as e:
         print(f"Error al iniciar la aplicación: {e}")
+
--- mvc/controller/registro_controller.py
+++ mvc/controller/registro_controller.py
@@ -2,17 +2,42 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from mvc.model.db_connection import create_connection, close_connection
 from mysql.connector import Error
 import bcrypt  # Importar bcrypt
+import re # Importar el módulo de expresiones regulares
 
 registro_controller = Blueprint('registro_controller', __name__)
 
 @registro_controller.route('/registro', methods=['GET', 'POST'])
 def registro():
+
+    from app import mail
+    from flask_mail import Message
+
     if request.method == 'POST':
         nombre = request.form['nombre']
         email = request.form['email']
         contrasena = request.form['contrasena'].encode('utf-8')  # Convertir a bytes
         nombreRol = 'Usuario'  # Asignar un rol predeterminado, como 'Usuario'
 
+        if not nombre.isalpha():
+            flash('El nombre solo debe contener letras.', 'error')
+            return redirect(url_for('registro_controller.registro'))
+        
+        
+        if len(nombre) < 4:
+            flash('El nombre debe tener al menos 4 caracteres.', 'error')
+            return redirect(url_for('registro_controller.registro'))
+        
+        
+        dominios_permitidos = ['@gmail.com', '@hotmail.com', '@yahoo.com', '@outlook.com', '@live.com']
+
+        if len(email) < 5 or '@' not in email or not any(email.endswith(dominio) for dominio in dominios_permitidos):
+            flash('Por favor, ingrese un correo electrónico válido' , 'error')
+            return redirect(url_for('registro_controller.registro'))
+        
+        if len(contrasena) < 4 or not any(c in '!@#$%^&*()-_=+[]{}|;:,.<>?/' for c in contrasena.decode('utf-8')):
+            flash('La contraseña debe tener al menos 4 caracteres y un carácter especial.', 'error')
+            return redirect(url_for('registro_controller.registro'))
+        
         # Encriptar la contraseña
         hashed_password = bcrypt.hashpw(contrasena, bcrypt.gensalt())
 
@@ -25,10 +50,17 @@ def registro():
                 (nombre, email, hashed_password.decode('utf-8'), '', '', nombreRol)  # Almacenar la contraseña encriptada
             )
             connection.commit()
-            flash("Usuario registrado con éxito. Por favor, inicia sesión.")  # Mensaje de éxito
+
+            msg = Message('Bienvenido a TearsOfMisery', recipients=[email])
+            msg.html = render_template('registro_email.html', nombre=nombre, email=email)
+            mail.send(msg)
+
+            flash('Usuario registrado con éxito. Por favor, inicia sesión.', 'exito')  # Mensaje de éxito
             return redirect(url_for('login_controller.login'))  # Usar el blueprint y endpoint correcto
+        
         except Error as e:
               # Mensaje de error
+            flash('Hubo un error al registrar el usuario.', 'error')  
             return redirect(url_for('registro_controller.registro'))  # Usar el blueprint y endpoint correcto
         finally:
             if connection and connection.is_connected():
