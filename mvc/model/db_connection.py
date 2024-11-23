6b46b634bbb9d117e15c12df72825934afa42eb2
Duksoon <sergioromerogus7@gmail.com>
2024-11-22 21:51:37 -0500

    Migracion de bd a railway a

--- mvc/model/db_connection.py
+++ mvc/model/db_connection.py
@@ -1,23 +1,18 @@
-import mysql.connector
-from mysql.connector import Error
+import MySQLdb
 from flask import current_app
-import MySQLdb as mysql
+import logging
 
 def create_connection():
     try:
-        connection = mysql.connector.connect(
+        connection = MySQLdb.connect(
             host=current_app.config['MYSQL_HOST'],
             user=current_app.config['MYSQL_USER'],
             password=current_app.config['MYSQL_PASSWORD'],
             database=current_app.config['MYSQL_DB'],
             port=current_app.config['MYSQL_PORT']
         )
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        print(f"Error al conectar a la base de datos: {e}")
+        logging.info("Database connection successful")
+        return connection
+    except MySQLdb.Error as e:
+        logging.error(f"Error connecting to MySQL Platform: {e}")
         return None
-
-def close_connection(connection):
-    if connection and connection.is_connected():
-        connection.close()
